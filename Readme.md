c0042508c111b2f5cb4201c3af444e2a256efb44
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# DXRichEdit for WPF: How to implement progress indicator for slow RichEditControl operations
+
+
+<p>This example demonstrates how to utilize the <strong>IProgressIndicationService </strong>service interface of the RichEditControl. The ProgressBar control on the form indicates how the mail merge operation proceeds.</p><br />
+
+
+<br/>
+
+
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,64 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports DevExpress.XtraRichEdit.API.Native
+Imports DevExpress.XtraRichEdit
+
+#Region "#usings"
+Imports DevExpress.Services
+#End Region ' #usings
+
+Namespace ProgressIndicator
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub richEditControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			richEditControl1.ApplyTemplate()
+			richEditControl1.LoadDocument("Docs\invitation.docx")
+			richEditControl1.Options.MailMerge.DataSource = New SampleData()
+		End Sub
+
+		Private Sub btnMailMerge_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim myMergeOptions As MailMergeOptions = richEditControl1.Document.CreateMailMergeOptions()
+			myMergeOptions.MergeMode = MergeMode.NewSection
+			richEditControl1.Document.MailMerge(myMergeOptions, richEditControl2.Document)
+			tabControl.SelectedIndex = 1
+		End Sub
+
+		Private Sub richEditControl1_MailMergeStarted(ByVal sender As Object, ByVal e As MailMergeStartedEventArgs)
+'			#Region "#servicesubst"
+			richEditControl1.RemoveService(GetType(IProgressIndicationService))
+			richEditControl1.AddService(GetType(IProgressIndicationService),  _
+		New MyProgressIndicatorService(richEditControl1, Me.progressBarControl1))
+'			#End Region ' #servicesubst
+		End Sub
+
+		Private Sub richEditControl1_MailMergeFinished(ByVal sender As Object, ByVal e As MailMergeFinishedEventArgs)
+			richEditControl1.RemoveService(GetType(IProgressIndicationService))
+		End Sub
+
+		Private Sub richEditControl1_MailMergeRecordStarted(ByVal sender As Object, ByVal e As MailMergeRecordStartedEventArgs)
+			' Imitating slow data fetching
+			System.Threading.Thread.Sleep(100)
+		End Sub
+
+		Private Sub richEditControl1_MailMergeRecordFinished(ByVal sender As Object, ByVal e As MailMergeRecordFinishedEventArgs)
+			e.RecordDocument.AppendDocumentContent("Docs\bungalow.docx", DocumentFormat.OpenXml)
+		End Sub
+
+		Private Sub tabControl_SelectionChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Core.TabControlSelectionChangedEventArgs)
+			Select Case tabControl.SelectedIndex
+				Case 0
+					Me.btnMailMerge.IsEnabled = True
+				Case 1
+					Me.btnMailMerge.IsEnabled = False
+			End Select
+		End Sub
+	End Class
+End Namespace
